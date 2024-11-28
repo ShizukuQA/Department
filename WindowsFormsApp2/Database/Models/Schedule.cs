3b984a9acadda4ccb12819c0bf3c658e2fafee45
ShizukuQA <Buzakov.kirill0@gmail.com>
2024-11-29 01:42:39 +0300

    feat: initial commit
    moving this application to a remote repository may adversely affect its performance

---
+++ WindowsFormsApp2/Database/Models/Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.Database.Models {
+    public class Schedule {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Classroom { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ WindowsFormsApp2/Pages/Admin/AddTeacher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp2.Database;
+using WindowsFormsApp2.Database.Models;
+
+namespace WindowsFormsApp2.Pages.Admin {
+    public partial class AddTeacher : Form {
+        private DatabaseContext db;
+
+        public AddTeacher(DatabaseContext db) {
+            InitializeComponent();
+            this.db = db;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e) {
+            try {
+                User teacher = new User() {
+                    Firstname = txtName.Text,
+                    SecondName = txtSurname.Text,
+                    Email = txtEmail.Text,
+                    Password = txtPassword.Text,
+                    IsAdmin = false
+                };
+                db.Users.Add(teacher);
+                MessageBox.Show("Преподаватель добавлен!");
+            }
+            catch {
+                MessageBox.Show("Неизвестная ошибка");
+            }
+        }
+    }
+}
