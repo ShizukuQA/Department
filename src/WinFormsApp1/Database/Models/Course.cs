abb7b491198e246011b09d7ce45002af38002f49
ShizukuQA <Buzakov.kirill0@gmail.com>
2024-12-12 17:52:14 +0300

    fix: creation of work files

---
+++ src/WinFormsApp1/Database/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Database.Models {
+    public class Course {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/WinFormsApp1/Pages/AdminForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Database;
+using WindowsFormsApp1.Pages.Admin;
+
+namespace WindowsFormsApp1.Pages {
+    public partial class AdminForm : Form {
+        private DatabaseContext db;
+
+        public AdminForm(DatabaseContext db) {
+            InitializeComponent();
+            this.db = db;
+        }
+
+        private void BtnCreateCourse_Click(object sender, EventArgs e) {
+            new CreateCourse(db).Show();
+        } 
+
+        private void BtnAddStudent_Click(object sender, EventArgs e) {
+            new AddStudent(db).Show();
+        } 
+
+        private void BtnEditSchedule_Click(object sender, EventArgs e) { 
+            new EditSchedule(db).Show();
+        }
+
+        private void BtnManageTeachers_Click(object sender, EventArgs e) { 
+            new ManageTeachers(db).Show();
+        }
+    }
+}
